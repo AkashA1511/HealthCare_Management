a21bc0b39257d081899b39f09a26a5cccbf9067e
Akash Athare <90034500+AkashA1511@users.noreply.github.com>
2024-04-01 12:24:49 +0530

    Add files via upload

---
+++ test/MedicalRecord.test.js
@@ -0,0 +1,10 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("MedicalRecord" ,()=>{
+    let user1,medical,transactionResponse,transactionRecipet;
+    beforeEach(async () => {
+        const accounts = await ethers.getSigner();
+    })
+
+})
